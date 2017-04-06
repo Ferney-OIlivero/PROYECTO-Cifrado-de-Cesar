b992a0d5c619632c572f4ec1015dc0451b8df24b
Ferney <deivido.1997@gmail.com>
2017-04-06 08:29:17 -0430

    aqui añadi la parte del caso 2, para decodificar lo que esta en el archivo preestablecido.

--- main.cpp
+++ main.cpp
@@ -1,54 +1,93 @@
 #include <iostream>
 #include <fstream>
 
+
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	
-	string palabra;
-	int numero,tam,opc;
+	string auxiliar,otra,texto;
+	int numero,tam,opc,cont=0;
 	ofstream myfile;
+	ifstream archivo;
 	
-	cout<<"\tSELECIONES UNA OPCION"<<endl;
-	cout<<"1.- CODIFICAR"<<endl;
-	cout<<"1.- DECODIFICAR"<<endl;
-	cin>>opc;
-	switch(opc){
-		case 1:
-			cout<<"INGRESE LA PALABRA QUE DESEA CODIFICAR"<<endl;
-			cin>>palabra;
-			cout<<"INGRESE El NUMERO DE LA CODIFICACION"<<endl;
-			cin>>numero;
+	do{
 	
-			tam=palabra.size();
-			for(int x=0; x<tam; x++){
-				palabra[x]=palabra[x]-((numero%26)-1);
-				
-				if(palabra[x]>90){
-					palabra[x]=palabra[x]-26;
-				}
-				if(palabra[x]<65){
-					palabra[x]=palabra[x]+26;
-				}
-			}
-			
-			cout<<"PALABRA CODIFICADA:    "<<palabra;
-	
-			
-			myfile.open ("codificado.txt",ios::trunc);
-			if(myfile.is_open()){
-				myfile<<palabra;
-			}
-			else{
-				cout<<"NO SE COPIO LA PALABRA"<<endl;
-			}
-			myfile.close();
-		break;
+		cout<<"\tSELECIONES UNA OPCION"<<endl;
+		cout<<"1.- CODIFICAR"<<endl;
+		cout<<"2.- DECODIFICAR"<<endl;
+		cout<<"3.- SALIR"<<endl;
+		cin>>opc;
+		switch(opc){
+			case 1:
+				cout<<"INGRESE EL TEXTO QUE DESEA CODIFICAR"<<endl;
+				do{
+					fflush(stdin);
+					getline(cin,otra);
+					if(otra!=":SACAME"){
+						auxiliar=otra;
+					}
+					else{
+						auxiliar="";
+					}
+					texto=texto+auxiliar+'\n';
+				}while(otra!=":SACAME");
+				tam=(texto.size());		
+				cout<<"ESCRITO:\n"<<texto<<endl;
+				cout<<"INGRESE El NUMERO DE LA CODIFICACION"<<endl;
+				cin>>numero;
 		
-		case 2:
-			cout<<"";
-		break;
-	}
+				for(int x=0; x<tam; x++){
+					if(texto[x]>64 && texto[x]<91){
+						texto[x]=texto[x]-((numero%26)-1);
+						if(texto[x]<65){
+							texto[x]=texto[x]+26;
+						}
+					}	
+				}
+			
+				cout<<"TEXTO CODIFICADO:\n"<<texto;
+
+				myfile.open ("codificado.txt",ios::trunc);
+				if(myfile.is_open()){
+					myfile<<texto;
+				}
+				else{
+					cout<<"NO SE COPIO LA PALABRA"<<endl;
+				}
+				myfile.close();
+			break;
+		
+			case 2:
+				cout<<"";
+				cout<<"INGRESE El NUMERO DE LA DECODIFICACION"<<endl;
+				cin>>numero;
+				archivo.open("codificado.txt");
+				if(archivo.is_open()){
+					while(getline(archivo,texto)){
+						tam=texto.size();
+						for(int x=0; x<tam; x++){
+							if(texto[x]>64 && texto[x]<91){
+								texto[x]=texto[x]+((numero%26)-1);
+								if(texto[x]>90){
+									texto[x]=texto[x]-26;
+								}
+							}
+						}
+						cout<<texto<<endl;
+					}
+				}
+				else{
+					cout<<"no se leyo el archivo";
+				}
+				archivo.close();
+			break;
+			
+			case 3:
+				return 0;
+			break;
+		}
+	}while(opc!=3);
 	return 0;
 }
